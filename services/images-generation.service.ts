30375674a7366675ecfc11bc65a1cb353af5582b
Favio Dileva <A01745465@tec.mx>
2023-03-19 23:06:50 -0600

    final

--- services/images-generation.service.ts
+++ services/images-generation.service.ts
@@ -1,6 +1,8 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class ImagesGenerationService {
     const url = 'https://api.openai.com/v1/images/generations';
     const headers = new HttpHeaders()
       .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer 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');
+      .set('Authorization', `Bearer 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`);
 
     const requestBody = {
       'model': 'image-alpha-001',
